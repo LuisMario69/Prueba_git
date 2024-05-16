eb29ab37a65cdd369deed728d0f055e65982da34
Luis Alzate <saizmateo96@gmail.com>
2024-05-16 16:30:28 -0500

    Herencia y Polimorfismo - Implementación de Subclases por Luis

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,6 +1,7 @@
 class Gerente extends Empleado {
     private double bono;
 
+    // Polimorfismo
     @Override
     public double calcularSalario() {
         return super.calcularSalario() + bono;
